d2b4c30d09d1b292bb50f47cdad8fb2952bf8c55
abdfaruk <farukabdullahal9@gmail.com>
2022-02-08 23:50:35 +0600

    some changes with navbar

--- src/Pages/Nav/Nav.js
+++ src/Pages/Nav/Nav.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './Nav.css';
 
 const Nav = () => {
     return (
         <div>
             <div className='container mx-auto  flex items-center justify-between p-5  '>
                 <div>
-                    <NavLink to='/'>Abdullah</NavLink>
+                    <NavLink to='/' className='nav-logo'>Abdullah</NavLink>
                 </div>
-                <div className='space-x-2'>
-                    <NavLink to='/blog'>Blog</NavLink>
-                    <NavLink to='/project'>Project</NavLink>
-                    <NavLink to='/about'>About</NavLink>
-                    <NavLink to='/contact'>Contact</NavLink>
+                <div className='space-x-2 navstyle'>
+                    <NavLink to='/blog' className='blog'>Blog</NavLink>
+                    <NavLink to='/project' className='blog'>Project</NavLink>
+                    <NavLink to='/about' className='blog'>About</NavLink>
+                    <NavLink to='/contact' className='blog'>Contact</NavLink>
                 </div>
             </div>
         </div>
